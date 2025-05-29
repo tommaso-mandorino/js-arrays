6f2e4682717022efa159ab73faf634cffb2d3312
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-29 17:34:59 +0200

    feat(02-arrays-bonus): finish task 1

--- js-arrays/02-arrays-bonus/script.js
+++ js-arrays/02-arrays-bonus/script.js
@@ -10,7 +10,11 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+for(let i = 0; i < teachers.length; i++) {
+  reversedTeachers.unshift(teachers[i]);
+}
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
