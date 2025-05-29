1104d008da99505c90607c375e5c9278fb0c2c7d
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-29 17:49:18 +0200

    feat(02-arrays-bonus): finish task 4

--- js-arrays/02-arrays-bonus/script.js
+++ js-arrays/02-arrays-bonus/script.js
@@ -33,7 +33,8 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio') + 1 ? true : false;
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
