4e05cf78f02767f90f44fbf545f7ee1532b82aa6
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-29 17:27:45 +0200

    feat(01-arrays): finish task 8

--- js-arrays/01-arrays/script.js
+++ js-arrays/01-arrays/script.js
@@ -40,4 +40,5 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length ? false : true;
+console.log(isTeachersEmpty);
