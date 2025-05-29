b3cb0052b1059f032c13a7427a221bc01a1c893e
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-29 17:42:42 +0200

    feat(02-arrays-bonus): finish task 3

--- js-arrays/02-arrays-bonus/script.js
+++ js-arrays/02-arrays-bonus/script.js
@@ -28,6 +28,8 @@ for(let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf('Ed'), 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
